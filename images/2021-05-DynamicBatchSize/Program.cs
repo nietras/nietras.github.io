6b67ad7a1b05c3ebf5accf6c56f261558d557fee
ntr <ntr@ihfood.dk>
2021-05-13 21:07:37 +0200

    Run inference

--- images/2021-05-DynamicBatchSize/Program.cs
+++ images/2021-05-DynamicBatchSize/Program.cs
@@ -1,3 +1,16 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Onnx;
 
-Console.WriteLine("Hello World!");
+var modelPath = "mnist-8.onnx";
+var inference = new InferenceSession(modelPath);
+
+var inputs = inference.InputMetadata.Select(p => 
+    NamedOnnxValue.CreateFromTensor(p.Key, new DenseTensor<float>(p.Value.Dimensions)))
+    .ToArray();
+
+using var outputs = inference.Run(inputs);
+
+Console.WriteLine(string.Join(",", outputs.Select(o => $"{o.Name} {o.AsTensor<float>().Rank}")));
