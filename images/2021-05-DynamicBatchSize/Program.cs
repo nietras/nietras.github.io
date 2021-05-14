eb974defd95c6fcf1fe5b54b926261854e5be289
ntr <ntr@ihfood.dk>
2021-05-14 11:24:24 +0200

    if/else plus mean timing

--- images/2021-05-DynamicBatchSize/Program.cs
+++ images/2021-05-DynamicBatchSize/Program.cs
@@ -1,36 +1,55 @@
-﻿using System;
+﻿#define SET_DYNAMIC_BATCH_SIZE
+using System;
 using System.Diagnostics;
 using System.Linq;
+using Google.Protobuf;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Onnx;
 
-Action<string> log = Console.WriteLine;
-const int BatchSize = 1;
-const int Iterations = 7;
-
 var modelPath = "mnist-8.onnx";
+
+#if SET_DYNAMIC_BATCH_SIZE
+var model = ModelProto.Parser.ParseFromFile(modelPath);
+model.Graph.SetDim();
+var modelBytes = model.ToByteArray();
+
+using var inference = new InferenceSession(modelBytes);
+#else
 using var inference = new InferenceSession(modelPath);
+#endif
 
-var inputs = inference.InputMetadata.Select(p =>
-    NamedOnnxValue.CreateFromTensor(p.Key,
-        new DenseTensor<float>(SetBatchSize(p.Value.Dimensions, BatchSize))))
-    .ToArray<NamedOnnxValue>();
-
-for (int i = 0; i < Iterations; i++)
+foreach (var batchSize in new[] { 1, 2, 4, 8 })
 {
-    var before = Stopwatch.GetTimestamp();
+    var inputs = inference.InputMetadata.Select(p =>
+        NamedOnnxValue.CreateFromTensor(p.Key,
+            new DenseTensor<float>(SetBatchSize(p.Value.Dimensions, batchSize))))
+        .ToArray<NamedOnnxValue>();
 
-    using var outputs = inference.Run(inputs);
+    const int iterations = 10;
+    const int skipFirst = 3;
+    var sum_ms = 0.0;
+    for (int i = 0; i < iterations; i++)
+    {
+        var before = Stopwatch.GetTimestamp();
 
-    var after = Stopwatch.GetTimestamp();
-    var time_ms = 1000.0 * (after - before) / Stopwatch.Frequency;
+        using var outputs = inference.Run(inputs);
 
-    var output = outputs.Single();
-    var outputTensor = output.AsTensor<float>();
-
-    log($"N={BatchSize,1} Tb={time_ms:F3}ms Ts={time_ms/BatchSize:F3}ms " +
-        $"{string.Join(",", outputTensor.Select(v => $"{v:F3}"))}");
+        if (i >= skipFirst)
+        {
+            var after = Stopwatch.GetTimestamp();
+            var time_ms = 1000.0 * (after - before) / Stopwatch.Frequency;
+            sum_ms += time_ms;
+            if (i == iterations - 1)
+            {
+                var output = outputs.Single();
+                var outputTensor = output.AsTensor<float>();
+                var arrayString = outputTensor.GetArrayString();
+                var mean_ms = sum_ms / (iterations - skipFirst);
+                Console.WriteLine($"N={batchSize} T={time_ms:F3}ms {arrayString}");
+            }
+        }
+    }
 }
 
 static int[] SetBatchSize(int[] dimensions, int batchSize)
