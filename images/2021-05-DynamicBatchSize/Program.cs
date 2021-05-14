d5452d4b9a0487e8996fcfb957c7803f3ba0b378
ntr <ntr@ihfood.dk>
2021-05-14 11:01:48 +0200

    Add a bit of timing and batch size overriding

--- images/2021-05-DynamicBatchSize/Program.cs
+++ images/2021-05-DynamicBatchSize/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Onnx;
 
+Action<string> log = Console.WriteLine;
+const int BatchSize = 1;
+const int Iterations = 7;
+
 var modelPath = "mnist-8.onnx";
-var inference = new InferenceSession(modelPath);
+using var inference = new InferenceSession(modelPath);
 
-var inputs = inference.InputMetadata.Select(p => 
-    NamedOnnxValue.CreateFromTensor(p.Key, new DenseTensor<float>(p.Value.Dimensions)))
-    .ToArray();
+var inputs = inference.InputMetadata.Select(p =>
+    NamedOnnxValue.CreateFromTensor(p.Key,
+        new DenseTensor<float>(SetBatchSize(p.Value.Dimensions, BatchSize))))
+    .ToArray<NamedOnnxValue>();
 
-using var outputs = inference.Run(inputs);
+for (int i = 0; i < Iterations; i++)
+{
+    var before = Stopwatch.GetTimestamp();
 
-Console.WriteLine(string.Join(",", outputs.Select(o => $"{o.Name} {o.AsTensor<float>().Rank}")));
+    using var outputs = inference.Run(inputs);
+
+    var after = Stopwatch.GetTimestamp();
+    var time_ms = 1000.0 * (after - before) / Stopwatch.Frequency;
+
+    var output = outputs.Single();
+    var outputTensor = output.AsTensor<float>();
+
+    log($"N={BatchSize,1} Tb={time_ms:F3}ms Ts={time_ms/BatchSize:F3}ms " +
+        $"{string.Join(",", outputTensor.Select(v => $"{v:F3}"))}");
+}
+
+static int[] SetBatchSize(int[] dimensions, int batchSize)
+{
+    dimensions[0] = batchSize;
+    return dimensions;
+}
