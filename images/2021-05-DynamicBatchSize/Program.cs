fbe9ff57df4d0c6f21a12615470869772125b701
ntr <ntr@ihfood.dk>
2021-05-24 18:56:05 +0200

    cleanup program.cs

--- images/2021-05-DynamicBatchSize/Program.cs
+++ images/2021-05-DynamicBatchSize/Program.cs
@@ -5,14 +5,12 @@ using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Onnx;
 
-var model = ModelProto.Parser.ParseFromFile("mnist-8.onnx"); //"squeezenet1.0-9.onnx");//"efficientnet-lite4-11.onnx"); /
-const int batchSize = 4;
-model.Graph.SetDim(dimIndex: 0, DimParamOrValue.New(4));
-model.WriteToFile("mnist-8-fixed-4-batchsize-onnx");
+var model = ModelProto.Parser.ParseFromFile("mnist-8.onnx");
+const int batchSize = 2;
+model.Graph.SetDim();
 var modelBytes = model.ToByteArray();
 
 using var inference = new InferenceSession(modelBytes);
-
 
 var inputs = inference.InputMetadata.Select(p =>
         CreateNamedOnnxValueTensor(p.Key, p.Value, batchSize))
@@ -34,45 +32,3 @@ static NamedOnnxValue CreateNamedOnnxValueTensor(
     var tensor = new DenseTensor<float>(dimensions);
     return NamedOnnxValue.CreateFromTensor(name, tensor);
 }
-
-//#define SET_DYNAMIC_BATCH_SIZE
-//using System;
-//using System.Diagnostics;
-//using System.Linq;
-//using Google.Protobuf;
-//using Microsoft.ML.OnnxRuntime;
-//using Microsoft.ML.OnnxRuntime.Tensors;
-//using Onnx;
-
-//var modelPath = "mnist-8.onnx";
-
-//#if SET_DYNAMIC_BATCH_SIZE
-//var model = ModelProto.Parser.ParseFromFile(modelPath);
-//model.Graph.SetDim();
-//var modelBytes = model.ToByteArray();
-
-//using var inference = new InferenceSession(modelBytes);
-//#else
-//using var inference = new InferenceSession(modelPath);
-//#endif
-
-//const int batchSize = 1;
-
-//var inputs = inference.InputMetadata.Select(p =>
-//    NamedOnnxValue.CreateFromTensor(p.Key,
-//        new DenseTensor<float>(SetBatchSize(p.Value.Dimensions, batchSize))))
-//    .ToArray<NamedOnnxValue>();
-
-//using var outputs = inference.Run(inputs);
-
-//var output = outputs.Single();
-//var outputTensor = output.AsTensor<float>();
-//var arrayString = outputTensor.GetArrayString();
-
-//Console.WriteLine($"N={batchSize} {arrayString}");
-
-//static int[] SetBatchSize(int[] dimensions, int batchSize)
-//{
-//    dimensions[0] = batchSize;
-//    return dimensions;
-//}
