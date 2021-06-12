adfc2986eeb9e0253b4af78517c8be64d2274365
ntr <ntr@ihfood.dk>
2021-06-12 11:55:23 +0200

    simplify and add EquatableStruct

--- images/2021-06-Microsoft-build-2021/RecordStructBenchmark/Program.cs
+++ images/2021-06-Microsoft-build-2021/RecordStructBenchmark/Program.cs
@@ -1,10 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-namespace RecordStructBenchmark
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+
+public readonly struct Struct
 {
-    public class Program
+    public Struct(Type type, int value)
     {
-        public static void Main(string[] args) => 
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        Type = type;
+        Value = value;
     }
+
+    public Type Type { get; }
+    public int Value { get; }
+}
+
+public readonly struct EquatableStruct : IEquatable<EquatableStruct>
+{
+    public EquatableStruct(Type type, int value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public Type Type { get; }
+    public int Value { get; }
+
+    public bool Equals(EquatableStruct other) =>
+        Type.Equals(other.Type) && Value.Equals(other.Value);
+}
+
+public record struct RecordStruct(Type Type, int Value);
+
+public class StructDictionary
+{
+    readonly Dictionary<Struct, long> _structToValue = new()
+    {
+        { new Struct(typeof(byte), 101), 0 },
+        { new Struct(typeof(sbyte), 102), 1 },
+        { new Struct(typeof(ushort), 201), 2 },
+        { new Struct(typeof(short), 202), 3 },
+        { new Struct(typeof(uint), 301), 4 },
+        { new Struct(typeof(int), 302), 5 },
+    };
+
+    readonly Dictionary<EquatableStruct, long> _equatableStructToValue = new()
+    {
+        { new EquatableStruct(typeof(byte), 101), 0 },
+        { new EquatableStruct(typeof(sbyte), 102), 1 },
+        { new EquatableStruct(typeof(ushort), 201), 2 },
+        { new EquatableStruct(typeof(short), 202), 3 },
+        { new EquatableStruct(typeof(uint), 301), 4 },
+        { new EquatableStruct(typeof(int), 302), 5 },
+    };
+
+    readonly Dictionary<(Type, int), long> _valueTupleToValue = new()
+    {
+        { (typeof(byte), 101), 0 },
+        { (typeof(sbyte), 102), 1 },
+        { (typeof(ushort), 201), 2 },
+        { (typeof(short), 202), 3 },
+        { (typeof(uint), 301), 4 },
+        { (typeof(int), 302), 5 },
+    };
+
+    readonly Dictionary<RecordStruct, long> _recordStructToValue = new()
+    {
+        { new(typeof(byte), 101), 0 },
+        { new(typeof(sbyte), 102), 1 },
+        { new(typeof(ushort), 201), 2 },
+        { new(typeof(short), 202), 3 },
+        { new(typeof(uint), 301), 4 },
+        { new(typeof(int), 302), 5 },
+    };
+
+    readonly Struct _structKey = new (typeof(ushort), 201);
+    readonly EquatableStruct _equatableStructKey = new(typeof(ushort), 201);
+    readonly RecordStruct _recordStructKey = new (typeof(ushort), 201);
+    readonly (Type, int) _valueTupleKey = (typeof(ushort), 201);
+
+    [Benchmark(Baseline = true)]
+    public long Struct_() => _structToValue[_structKey];
+
+    [Benchmark()]
+    public long EquatableStruct_() => _equatableStructToValue[_equatableStructKey];
+
+    [Benchmark]
+    public long ValueTuple_() => _valueTupleToValue[_valueTupleKey];
+
+    [Benchmark()]
+    public long RecordStruct_() => _recordStructToValue[_recordStructKey];
 }
--- images/2021-06-Microsoft-build-2021/RecordStructBenchmark/StructDictionary.cs
+++ images/2021-06-Microsoft-build-2021/RecordStructBenchmark/StructDictionary.cs
@@ -7,19 +7,6 @@ using BenchmarkDotNet.Attributes;
 
 namespace RecordStructBenchmark
 {
-    public readonly struct Struct
-    {
-        public Struct(Type type, int value)
-        {
-            Type = type;
-            Value = value;
-        }
-
-        public Type Type { get; }
-        public int Value { get; }
-    }
-
-    public record struct RecordStruct(Type Type, int Value);
 
     // https://sharplab.io/#v2:EYLgZgpghgLgrgJwgZwLRIMYHsEBNXIwJwYzIA+AAgEwCMAsAFBMBuUCABAA4cC8HAOwgB3DgAUsASwExqAEQ4BvDgA0+HWgBoOATXXUOAXwDcrdhwCO6nsMkwAFktXqAzEdOMJ02XID8HTn4LADoAUQs4KAAbZAAKLgBKDn8eEEE4KKiPSloATliEBI8mSjdMHFwOQmJScSkZeViBb1VtZpldIqYSsohsPCqiEhgOACU+ioBlIdJYgBUATy4IDkXl7RaANWi4CC7GIA
     //public struct RecordStruct : IEquatable<RecordStruct>
@@ -130,50 +117,4 @@ namespace RecordStructBenchmark
     //        Value = this.Value;
     //    }
     //}
-
-    public class StructDictionary
-    {
-        Dictionary<Struct, long> _structToValue = new ()
-        {
-            { new Struct(typeof(byte), 101), 0},
-            { new Struct(typeof(sbyte), 102), 1},
-            { new Struct(typeof(ushort), 201), 2},
-            { new Struct(typeof(short), 202), 3},
-            { new Struct(typeof(uint), 301), 4},
-            { new Struct(typeof(int), 302), 5},
-        };
-
-        Dictionary<(Type, int), long> _valueTupleToValue = new ()
-        {
-            { (typeof(byte), 101), 0},
-            { (typeof(sbyte), 102), 1},
-            { (typeof(ushort), 201), 2},
-            { (typeof(short), 202), 3},
-            { (typeof(uint), 301), 4},
-            { (typeof(int), 302), 5},
-        };
-
-        Dictionary<RecordStruct, long> _recordStructToValue = new()
-        {
-            { new (typeof(byte), 101), 0 },
-            { new (typeof(sbyte), 102), 1 },
-            { new (typeof(ushort), 201), 2 },
-            { new (typeof(short), 202), 3 },
-            { new (typeof(uint), 301), 4 },
-            { new (typeof(int), 302), 5 },
-        };
-
-        readonly Struct _structKey = new Struct(typeof(ushort), 201);
-        readonly RecordStruct _recordStructKey = new RecordStruct(typeof(ushort), 201);
-        readonly (Type, int) _valueTupleKey = (typeof(ushort), 201);
-
-        [Benchmark(Baseline = true)]
-        public long Struct_() => _structToValue[_structKey];
-
-        [Benchmark()]
-        public long RecordStruct_() => _recordStructToValue[_recordStructKey];
-
-        [Benchmark]
-        public long ValueTuple_() => _valueTupleToValue[_valueTupleKey];
-    }
 }
