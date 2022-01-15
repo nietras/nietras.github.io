73656f6434228bcecaac671ebbcb52e96885c349
nietras <nietras@users.noreply.github.com>
2022-01-15 19:26:22 +0100

    Bending .NET - Corrected Common Flat Build Output (#20)

---
+++ images/2022-01-bendingdotnet-corrected-common-flat-build-output/src/CommonFlatBuild.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonFlatBuild.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var c = new Class1();
+    }
+}
