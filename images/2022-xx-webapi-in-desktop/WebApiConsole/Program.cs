f50ea36eb983d4b15950fd3e4dfd11e1eb40a9cb
ntr <ntr@ihfood.dk>
2022-09-23 11:00:22 +0200

    make annotations work

--- images/2022-xx-webapi-in-desktop/WebApiConsole/Program.cs
+++ images/2022-xx-webapi-in-desktop/WebApiConsole/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.Diagnostics;
+using Swashbuckle.AspNetCore.Annotations;
 
 Action<string> log = t => { Trace.WriteLine($"T{Environment.CurrentManagedThreadId:D2} {t}"); };
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => o.EnableAnnotations());
 builder.WebHost.UseUrls("http://localhost:8080");
 
 var app = builder.Build();
@@ -27,7 +28,11 @@ var slidersStore = new SlidersStore(new Dictionary<string, int>()
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/sliders", async () =>
+app.MapGet("/sliders",
+[SwaggerOperation(Summary = "Returns current sliders", Description = "GET DESCRIPTION")]
+//[SwaggerResponse(200, "success")]
+//[SwaggerResponse(500, "some failure")]
+async () =>
 {
     log("GET /sliders");
     var tcs = new TaskCompletionSource<IReadOnlyDictionary<string, int>>();
@@ -40,16 +45,25 @@ app.MapGet("/sliders", async () =>
 })
 .WithName("GetSliders");
 
-app.MapPut("/sliders", async (Dictionary<string, int> newSliders) =>
+app.MapPut("/sliders",
+[SwaggerOperation(Summary = "Set new sliders", Description = "PUT DESCRIPTION")]
+//[SwaggerResponse(200, "success")]
+//[SwaggerResponse(500, "some failure")]
+async (Dictionary<string, int> newSliders) =>
 {
     log("PUT /sliders");
     var tcs = new TaskCompletionSource();
+    // Put can return state of sliders after if we want to be sure web client uses latest state after put, if that could be different
+    //var tcs = new TaskCompletionSource<IReadOnlyDictionary<string, int>>();
 
 #pragma warning disable CS4014 // Ignore Task returned here since we just use Task.Run as hack of sending action to another thread
     Task.Run(() => PutSliders(newSliders, tcs));
 #pragma warning restore CS4014
 
     await tcs.Task;
+
+    // if put returns latest state
+    //return await tcs.Task;
 })
 .WithName("PutSliders");
 
