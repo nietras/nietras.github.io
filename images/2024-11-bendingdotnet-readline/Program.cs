71a1fc1078a05d95bb011c21d7b0b96612027e40
nietras <nietras@users.noreply.github.com>
2024-11-29 12:22:35 +0100

    full post

--- images/2024-11-bendingdotnet-readline/Program.cs
+++ images/2024-11-bendingdotnet-readline/Program.cs
@@ -5,57 +5,46 @@ using System.IO;
 Action<string?> log = Console.WriteLine;
 var text = "1\n2\n3\n";
 
-log("SYNC");
-
-log("classic"); // one ReadLine call, out of loop variable, inside loop assignment
+log("SYNC - classic");
 {
     using var reader = new StringReader(text);
     string? line;
-    while ((line = reader.ReadLine()) != null)
-    { log(line); }
+    while ((line = reader.ReadLine()) != null) { log(line); }
 }
-log("is string"); // no out of loop variable, implicit null check, explicit type
+log("SYNC - is string");
 {
     using var reader = new StringReader(text);
-    while (reader.ReadLine() is string line)
-    { log(line); }
+    while (reader.ReadLine() is string line) { log(line); }
 }
-log("is {}"); // no out of loop variable, implicit null check, implicit type
+log("SYNC - is {}");
 {
     using var reader = new StringReader(text);
-    while (reader.ReadLine() is { } line)
-    { log(line); }
+    while (reader.ReadLine() is { } line) { log(line); }
 }
-log("");
 
-log("ASYNC");
-
-log("classic"); // one ReadLine call, out of loop variable, inside loop assignment
+log("ASYNC - classic");
 {
     using var reader = new StringReader(text);
     string? line;
-    while ((line = await reader.ReadLineAsync()) != null)
-    { log(line); }
+    while ((line = await reader.ReadLineAsync()) != null) { log(line); }
 }
-log("is string"); // no out of loop variable, implicit null check, explicit type
+log("ASYNC - is string");
 {
     using var reader = new StringReader(text);
-    while (await reader.ReadLineAsync() is string line)
-    { log(line); }
+    while (await reader.ReadLineAsync() is string line) { log(line); }
 }
-log("is {}"); // no out of loop variable, implicit null check, implicit type
+log("ASYNC - is {}");
 {
     using var reader = new StringReader(text);
-    while (await reader.ReadLineAsync() is { } line)
-    { log(line); }
+    while (await reader.ReadLineAsync() is { } line) { log(line); }
 }
 log("DONE");
 
-// Infinite loop version since `is var` does not perform implicit null check
+// Infinite loop version since `is var`
+// does not perform implicit null check
 var infinite = false;
 if (infinite)
 {
     using var reader = new StringReader(text);
-    while (reader.ReadLine() is var line)
-    { log(line); }
+    while (reader.ReadLine() is var line) { log(line); }
 }
